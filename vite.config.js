41b3823cc5b3103fd16c043700c491aa0553d9c2
Akmaliyah <2400016090@webmail.uad.ac.id>
2025-12-12 11:03:45 +0700

    setup deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/2400016090_tekwebprak7_ruang_ekspresi/',
   plugins: [react(),tailwindcss()],
   
 })
